3c2d90ad22dd10663fa78b3b80dfec3aa2ae0717
Bogdan Beley <bogdan.beley@outlook.com>
2020-08-14 19:36:57 +0300

    Add project files.

---
+++ L01_AuthorizationAndAuthentication_HW1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="L01_AuthorizationAndAuthentication_HW1.MvcApplication" Language="C#" %>
---
+++ L01_AuthorizationAndAuthentication_HW1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(L01_AuthorizationAndAuthentication_HW1.Startup))]
+namespace L01_AuthorizationAndAuthentication_HW1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
