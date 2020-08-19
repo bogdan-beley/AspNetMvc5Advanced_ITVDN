9ab67a9496b592adcc66c9177d2a166a6840d43d
Bogdan Beley <bogdan.beley@outlook.com>
2020-08-19 21:22:10 +0300

    L06_Lifecycle_HW1

---
+++ L06_Lifecycle_HW1/Modules/TimerModule.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+
+namespace L06_Lifecycle_HW1.Modules
+{
+    public class TimerModule : IHttpModule
+    {
+        private Stopwatch timer;
+
+        public void Init(HttpApplication app)
+        {
+            app.BeginRequest += HandleBeginRequest;
+            app.EndRequest += HandleEndRequest;
+        }
+
+        private void HandleBeginRequest(object sender, EventArgs e)
+        {
+            timer = Stopwatch.StartNew();
+        }
+
+        private void HandleEndRequest(object sender, EventArgs e)
+        {
+            HttpContext context = HttpContext.Current;
+            context.Response.Write($"На виконання запиту затрачено {((float)timer.ElapsedTicks / Stopwatch.Frequency)} секунд.");
+        }
+
+        public void Dispose() { }
+    }
+}
